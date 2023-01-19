a37ba3ad4849730cfa8d058cdeb61a1e4d11a3d1
seonysun <csh99240@naver.com>
2023-01-19 16:36:37 +0900

    team3_service

--- Lv1_Main_Service_Reserve/src/main/java/com/sist/dao/ServiceDAO.java
+++ Lv1_Main_Service_Reserve/src/main/java/com/sist/dao/ServiceDAO.java
@@ -2,11 +2,28 @@ package com.sist.dao;
 import java.util.*;
 import java.sql.*;
 import com.sist.vo.*;
+/* ask
+GANO       NOT NULL NUMBER         
+SUBJECT    NOT NULL VARCHAR2(1000) 
+TYPE       NOT NULL VARCHAR2(20)   
+CONTENT    NOT NULL CLOB           
+PWD        NOT NULL VARCHAR2(20)   
+REGDATE             DATE           
+ANS_STATE           VARCHAR2(20)   
+HIT                 NUMBER         
+GROUP_ID   NOT NULL NUMBER         
+GROUP_STEP          NUMBER         
+GROUP_TAB           NUMBER         
+MSG        NOT NULL CLOB           
+ID                  VARCHAR2(20)  
 
-/*
- * private int gano,hit,group_id,group_step,group_tab;
-	private String subject,type,content,pwd,ans_state,msg,dbday,id;
-	private Date regdate;*/
+faq
+GFNO    NOT NULL NUMBER         
+TYPE    NOT NULL VARCHAR2(20)   
+SUBJECT NOT NULL VARCHAR2(1000) 
+CONTENT NOT NULL CLOB           
+HIT              NUMBER    
+ * */
 public class ServiceDAO {
 	private Connection conn;
 	private PreparedStatement ps;
@@ -15,9 +32,9 @@ public class ServiceDAO {
 		ArrayList<AskVO> list=new ArrayList<AskVO>();
 		try {
 			conn=CreateConnection.getConnection();
-			String sql="SELECT gano,id,subject,type,ans_state,group_tab,hit,num "
-					+ "FROM (SELECT gano,id,subject,type,ans_state,group_tab,hit,rownum as num "
-					+ "FROM (SELECT gano,id,subject,type,ans_state,group_tab,hit "
+			String sql="SELECT gano,id,subject,type,ans_state,group_tab,hit,TO_CHAR(regdate,'YYYY-MM-DD'),num "
+					+ "FROM (SELECT gano,id,subject,type,ans_state,group_tab,hit,regdate,rownum as num "
+					+ "FROM (SELECT gano,id,subject,type,ans_state,group_tab,hit,regdate "
 					+ "FROM god_ask_3 "
 					+ "ORDER BY group_id DESC, group_step ASC)) "
 					+ "WHERE num BETWEEN ? AND ?";
@@ -37,6 +54,7 @@ public class ServiceDAO {
 				vo.setAns_state(rs.getString(5));
 				vo.setGroup_tab(rs.getInt(6));
 				vo.setHit(rs.getInt(7));
+				vo.setDbday(rs.getString(8));
 				list.add(vo);
 			}
 			rs.close();
@@ -52,8 +70,12 @@ public class ServiceDAO {
 		int total=0;
 		try {
 			conn=CreateConnection.getConnection();
-			String sql="";
+			String sql="SELECT CEIL(COUNT(*)/10.0) FROM god_ask_3";
 			ps=conn.prepareStatement(sql);
+			ResultSet rs=ps.executeQuery();
+			rs.next();
+			total=rs.getInt(1);
+			rs.close();
 		} catch(Exception ex) {
 			ex.printStackTrace();
 		} finally {
@@ -67,7 +89,30 @@ public class ServiceDAO {
 		try {
 			conn=CreateConnection.getConnection();
 			String sql="";
+			if(type==1) {
+				sql="UPDATE god_ask_3 "
+						+ "SET hit=hit+1 "
+						+ "WHERE gano=?";
+				ps=conn.prepareStatement(sql);
+				ps.setInt(1, no);
+				ps.executeUpdate();
+			}
+			sql="SELECT gano,id,subject,type,content,ans_state,TO_CHAR(regdate,'YYYY-MM-DD'),hit "
+					+ "FROM god_ask_3 "
+					+ "WHERE gano=?";
 			ps=conn.prepareStatement(sql);
+			ps.setInt(1, no);
+			ResultSet rs=ps.executeQuery();
+			rs.next();
+			vo.setGano(rs.getInt(1));
+			vo.setId(rs.getString(2));
+			vo.setSubject(rs.getString(3));
+			vo.setType(rs.getString(4));
+			vo.setContent(rs.getString(5));
+			vo.setAns_state(rs.getString(6));
+			vo.setDbday(rs.getString(7));
+			vo.setHit(rs.getInt(8));
+			rs.close();
 		} catch(Exception ex) {
 			ex.printStackTrace();
 		} finally {
@@ -79,8 +124,16 @@ public class ServiceDAO {
 	public void qnaInsert(AskVO vo) {
 		try {
 			conn=CreateConnection.getConnection();
-			String sql="";
+			String sql="INSERT INTO god_ask_3(gano,id,pwd,subject,type,content,group_id) "
+					+ "VALUES(ga_gano_sql.nextval,?,?,?,?,?,(SELECT NVL(MAX(group_id)+1,1) "
+					+ "FROM god_ask_3))";
 			ps=conn.prepareStatement(sql);
+			ps.setString(1, vo.getId());
+			ps.setString(2, vo.getPwd());
+			ps.setString(3, vo.getSubject());
+			ps.setString(4, vo.getType());
+			ps.setString(5, vo.getContent());
+			ps.executeUpdate();
 		} catch(Exception ex) {
 			ex.printStackTrace();
 		} finally {
@@ -88,11 +141,23 @@ public class ServiceDAO {
 		}
 	}
 	//QNA 답변
-	public void qnaReplyInsert(int no, String id) {
+	public void qnaReplyInsert(int no, AskVO vo) {
 		try {
 			conn=CreateConnection.getConnection();
-			String sql="";
+			String sql="SELECT group_id,group_step,group_tab "
+					+ "FROM god_ask_3 "
+					+ "WHERE gano=?";
 			ps=conn.prepareStatement(sql);
+			ps.setInt(1, no);
+			ResultSet rs=ps.executeQuery();
+	  		rs.next();
+	  		AskVO rvo=new AskVO();
+	  		rvo.setGroup_id(rs.getInt(1));
+	  		rvo.setGroup_step(rs.getInt(2));
+	  		rvo.setGroup_tab(rs.getInt(3));
+	  		rs.close();
+	  		
+	  		
 		} catch(Exception ex) {
 			ex.printStackTrace();
 		} finally {
@@ -100,27 +165,88 @@ public class ServiceDAO {
 		}
 	}
 	//QNA 수정
-	public void qnaUpdate(AskVO vo) {
+	public boolean qnaUpdate(AskVO vo) {
+		boolean bCheck=false;
 		try {
 			conn=CreateConnection.getConnection();
-			String sql="";
+			String sql="SELECT pwd FROM god_ask_3 "
+					+ "WHERE gano=?";
 			ps=conn.prepareStatement(sql);
+			ps.setInt(1, vo.getGano());
+			ResultSet rs=ps.executeQuery();
+			rs.next();
+			String db_pwd=rs.getString(1);
+			rs.close();
+			
+			if(db_pwd.equals(vo.getPwd())) {
+				bCheck=true;
+				sql="UPDATE god_ask_3 "
+						+ "SET subject=?, content=?, type=? "
+						+ "WHERE gano=?";
+				ps=conn.prepareStatement(sql);
+				ps.setString(1, vo.getSubject());
+				ps.setString(2, vo.getContent());
+				ps.setString(3, vo.getType());
+				ps.setInt(4, vo.getGano());
+				ps.executeUpdate();
+			}
 		} catch(Exception ex) {
 			ex.printStackTrace();
 		} finally {
 			CreateConnection.disConnection(conn, ps);
 		}
+		return bCheck;
 	}
 	//QNA 삭제
-	public void qnaDelete(int no, String pwd) {
+	public boolean qnaDelete(int no, String pwd) {
+		boolean bCheck=false;
 		try {
 			conn=CreateConnection.getConnection();
-			String sql="";
+			String sql="SELECT pwd,root,depth FROM god_ask_3 "
+					+ "WHERE gano=?";
 			ps=conn.prepareStatement(sql);
+			ps.setInt(1, no);
+			ResultSet rs=ps.executeQuery();
+			rs.next();
+			String db_pwd=rs.getString(1);
+			int root=rs.getInt(2);
+			int depth=rs.getInt(3);
+			rs.close();
+			
+			if(db_pwd.equals(pwd)) {
+				bCheck=true;
+				if(depth==0) {
+					sql="DELETE FROM god_ask_3 "
+							+ "WHERE gano=?";
+					ps=conn.prepareStatement(sql);
+					ps.setInt(1, no);
+					ps.executeUpdate();
+				} else {
+					String msg="삭제된 게시물입니다";
+					sql="UPDATE god_ask_3 "
+							+ "SET subject=?, content=? "
+							+ "WHERE gano=?";
+					ps=conn.prepareStatement(sql);
+					ps.setString(1, msg);
+					ps.setString(2, msg);
+					ps.setInt(3, no);
+					ps.executeUpdate();
+				}
+			}
+			
+			if(root!=0) {
+				sql="UPDATE god_ask_3 "
+						+ "SET depth=depth-1 "
+						+ "WHERE gano=?";
+				ps=conn.prepareStatement(sql);
+				ps.setInt(1, root);
+				ps.executeUpdate();
+			}
 		} catch(Exception ex) {
 			ex.printStackTrace();
 		} finally {
 			CreateConnection.disConnection(conn, ps);
 		}
+		return bCheck;
 	}
 }
--- Lv1_Main_Service_Reserve/src/main/java/com/sist/model/ServiceModel.java
+++ Lv1_Main_Service_Reserve/src/main/java/com/sist/model/ServiceModel.java
@@ -22,4 +22,42 @@ public class ServiceModel {
 		request.setAttribute("main_jsp", "../service/list.jsp");
 		return "../main/main.jsp";
 	}
+	
+	@RequestMapping("service/detail.do")
+	public String qna_detail(HttpServletRequest request, HttpServletResponse response) {
+		String fno=request.getParameter("fno");
+		ServiceDAO dao=new ServiceDAO();
+		AskVO vo=dao.qnaDetailData(Integer.parseInt(fno), 0);
+		request.setAttribute("vo", vo);
+		request.setAttribute("main_jsp", "../service/detail.jsp");
+		return "../main/main.jsp";
+	}
+	
+	@RequestMapping("serviec/reply.do")
+	public String qna_reply(HttpServletRequest request, HttpServletResponse response) {
+		
+		request.setAttribute("main_jsp", "../service/reply.jsp");
+		return "../main/main.jsp";
+	}
+	
+	@RequestMapping("serviec/insert.do")
+	public String qna_insert(HttpServletRequest request, HttpServletResponse response) {
+		
+		request.setAttribute("main_jsp", "../service/insert.jsp");
+		return "../main/main.jsp";
+	}
+	
+	@RequestMapping("serviec/update.do")
+	public String qna_update(HttpServletRequest request, HttpServletResponse response) {
+		
+		request.setAttribute("main_jsp", "../service/update.jsp");
+		return "../main/main.jsp";
+	}
+	
+	@RequestMapping("serviec/delete.do")
+	public String qna_delete(HttpServletRequest request, HttpServletResponse response) {
+		
+		request.setAttribute("main_jsp", "../service/delete.jsp");
+		return "../main/main.jsp";
+	}
 }
