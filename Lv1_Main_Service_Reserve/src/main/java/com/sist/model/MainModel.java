b989597aab693d675da435a37d6771266f3ae203
seonysun <csh99240@naver.com>
2023-01-21 00:19:19 +0900

    team3_controller set

--- Lv1_Main_Service_Reserve/src/main/java/com/sist/model/MainModel.java
+++ Lv1_Main_Service_Reserve/src/main/java/com/sist/model/MainModel.java
@@ -2,19 +2,19 @@ package com.sist.model;
 
 import com.sist.controller.Controller;
 import com.sist.controller.RequestMapping;
+import com.sist.dao.*;
+import com.sist.vo.*;
 
 import java.util.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
 @Controller
 public class MainModel {
 	@RequestMapping("main/main.do")
 	public String main_page(HttpServletRequest request, HttpServletResponse response) {
-		// include할 파일명을 전송
-		request.setAttribute("main_jsp", "../main/home.jsp"); // main.jsp
+		request.setAttribute("main_jsp", "../main/home.jsp");
 		return "../main/main.jsp";
 	}
 }
